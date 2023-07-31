dd41800249e6b1f0fec6e5a6c5ce5f1f524fda5b
Fatih Çetiner <f-cetiner99@hotmail.com>
2023-08-01 02:23:14 +0300

    The project was built and the first commit

---
+++ app/src/main/java/com/example/composemovieapp/presentation/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.composemovieapp.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.composemovieapp.presentation.ui.theme.ComposeMovieAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeMovieAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/composemovieapp/util/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.composemovieapp.util
+
+object Constants {
+
+    // Api key -> 31d72fc2
+    const val API_KEY = "31d72fc2"
+    const val BASE_URL = "http://www.omdbapi.com/"
+    const val IMDB_ID = "imdb_id"
+
+    //Page Name
+}
